71e547e43d02a1e659cfabb1626b82877eab5ad8
rowaxl <rowaxl0@gmail.com>
2020-09-03 15:40:23 -0700

    add login and booking trip function

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const { ApolloServer } = require('apollo-server');
+const isEmail = require('isemail');
 const typeDefs = require('./schema');
-const { createStore } = require('./utils')
-const resolvers = require('./resolvers')
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
 
 const store = createStore();
 
@@ -15,7 +16,17 @@ const server = new ApolloServer({
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({ store })
-  })
+  }),
+  context: async ({ req }) => {
+    const auth = req.headers && req.headers.authorization || '';
+    const email = Buffer.from(auth, 'base64').toString('ascii');
+    if (!isEmail.validate(email)) return { user: null };
+
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = users && users[0] || null;
+
+    return { user: { ...user.dataValues } };
+  }
 });
 
 server.listen().then(({ url }) => {
